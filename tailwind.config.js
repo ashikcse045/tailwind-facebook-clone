ac06b0208f30d39969e0f45628ac8f5c5ca8515c
ashik <ashikcse321@gmail.com>
2023-04-25 17:06:13 +0600

    header nav and left sidenav done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#0165E1',
+        ofWhite: '#F0F2F5',
+        textPrimary: '#050505',
+        textSec: '#65676B',
+      },
+      fontFamily:{
+        roboto: "'Roboto', sans-serif",
+        poppins: "'Poppins', sans-serif",
+      }
+    },
   },
   plugins: [],
 }
